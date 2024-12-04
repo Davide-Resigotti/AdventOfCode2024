ba1c14d959f2221332cf499e74cfe8e9e099ac76
Davide-Resigotti <idavide.resi@gmail.com>
2024-12-04 16:25:19 +0100

    day4part1 copied by a user

--- day3/day3-part2.py
+++ day3/day3-part2.py
@@ -1,28 +1,23 @@
 import re
 
-def multiply(row):
+mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+control_pattern = r"(do\(\)|don't\(\))"
+full_pattern = rf"{mul_pattern}|{control_pattern}"
 
-    mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
-    control_pattern = r"(do\(\)|don't\(\))"
-    full_pattern = rf"{mul_pattern}|{control_pattern}"
+enabled = True
+total_sum = 0
 
-    enabled = True
-    total_sum = 0
+matches = re.findall(full_pattern, open("day3/input.txt").read())
 
-    matches = re.findall(full_pattern, row)
+for item in matches:
+    print(item)
 
-    for mul_a, mul_b, control in matches:
-        if control == "do()":
-            enabled = True
-        elif control == "don't()":
-            enabled = False
-        elif mul_a and mul_b and enabled:
-            total_sum += int(mul_a) * int(mul_b)
-
-    return total_sum
-
-with open("day3/test2.txt") as file:
-    result = 0
-    for row in file:
-        result += multiply(row)
-        print(f"Total sum: {result}")
+for mul_a, mul_b, control in matches:
+    if control == "do()":
+        enabled = True
+    elif control == "don't()":
+        enabled = False
+    elif mul_a and mul_b and enabled:
+        total_sum += int(mul_a) * int(mul_b)
+        
+print(total_sum)
